c647b0f5d1427492cfb242531059ba6e2e11d5d2
nicol-hawkins <nicolchawkins@gmail.com>
2020-07-13 12:14:57 -0700

    added art history quiz to portfolio

--- src/Carousel.js
+++ src/Carousel.js
@@ -22,5 +22,9 @@ export default () => (
       <img className="project-img" alt="to-do-list" src={require('./images/to-do-list.png')} />
       <h3 className="legend">To-Do List <a href="https://nicol-hawkins.github.io/vanilla-todo-list/">Demo</a></h3>
     </div>
+    <div>
+      <img className="project-img" alt="art-history-quiz" src={require('./images/art-history.png')} />
+      <h3 className="legend">Art History Quiz <a href="https://art-history-js-quiz.herokuapp.com/">Demo</a></h3>
+    </div>
   </Carousel>
 );
