676f2546c3af72fa9c68bc4a6836a4ddd41a52b7
nicol-hawkins <nicolchawkins@gmail.com>
2020-10-05 17:55:08 -0700

    added blank target attribute to projects

--- src/Carousel.js
+++ src/Carousel.js
@@ -8,27 +8,27 @@ export default () => (
   <Carousel autoPlay>
     <div>
       <img className="project-img" alt="herbal-antidote" src={require('./images/herbal-antidote.png')} />
-      <h3 className="legend">Herbal Antidote <a href="https://herbal-antidote.herokuapp.com/">Demo</a></h3>
+      <h3 className="legend">Herbal Antidote <a href="https://herbal-antidote.herokuapp.com/" target="_blank">Demo</a></h3>
     </div>
     <div>
       <img className="project-img" alt="react-graph" src={require('./images/react-graph.png')} />
-      <h3 className="legend">React Graph <a href="https://south-american-forests.herokuapp.com/">Demo</a></h3>
+      <h3 className="legend">React Graph <a href="https://south-american-forests.herokuapp.com/" target="_blank">Demo</a></h3>
     </div>
     <div>
       <img className="project-img" alt="the-scratchpost" src={require('./images/the-scratchpost.png')} />
-      <h3 className="legend">The ScratchPost <a href="http://the-scratchpost.herokuapp.com/projects/">Demo</a></h3>
+      <h3 className="legend">The ScratchPost <a href="http://the-scratchpost.herokuapp.com/projects/" target="_blank">Demo</a></h3>
     </div>
     <div>
       <img className="project-img" alt="to-do-list" src={require('./images/to-do-list.png')} />
-      <h3 className="legend">To-Do List <a href="https://nicol-hawkins.github.io/vanilla-todo-list/">Demo</a></h3>
+      <h3 className="legend">To-Do List <a href="https://nicol-hawkins.github.io/vanilla-todo-list/" target="_blank">Demo</a></h3>
     </div>
     <div>
       <img className="project-img" alt="art-history-quiz" src={require('./images/art-history.png')} />
-      <h3 className="legend">Art History Quiz <a href="https://art-history-js-quiz.herokuapp.com/">Demo</a></h3>
+      <h3 className="legend">Art History Quiz <a href="https://art-history-js-quiz.herokuapp.com/" target="_blank">Demo</a></h3>
     </div>
     <div>
       <img className="project-img" alt="art-history-quiz" src={require('./images/meme-generator.png')} />
-      <h3 className="legend">Meme Generator <a href="https://art-history-js-quiz.herokuapp.com/">Demo</a></h3>
+      <h3 className="legend">Meme Generator <a href="https://art-history-js-quiz.herokuapp.com/" target="_blank">Demo</a></h3>
     </div>
   </Carousel>
 );
